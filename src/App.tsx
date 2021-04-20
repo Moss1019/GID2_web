b89fb8919ad28ea436ffc20c0be20b26075e4f2e
Moss1019 <mossonthetree@gmai.com>
2021-04-20 20:21:07 +0200

    test cloud build

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      React app working from cloud
+    </div>
+  );
+}
+
+export default App;
